f5e16b1065d9f4572a7b23a1c026fd94a1f05b8b
Vladimir Udodenko <vladimyr.udodenko@gmail.com>
2021-07-27 15:00:25 +0300

    feature/Checkout-form: done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import Cart from "../components/main/Cart/Cart";
 import Logout from "../components/header/Logout";
 import BooksRoutes from "./books/BooksRoutes";
 import NotFound from "../components/NotFound";
+import Checkout from "../components/main/Cart/Checkout";
 
 function Routes() {
   return (
@@ -14,6 +15,7 @@ function Routes() {
       <Route path="/login" component={Logout} />
       <Route path="/cart" component={Cart} />
       <Route exact path="/" render={() => <Redirect to="/books" />} />
+      <Route path="/checkout" component={Checkout} />
       <Route path="*" component={NotFound} />
     </Switch>
   );
